546c8f037c46c6f9a6c35685fa13e3527cc8f540
arinenn <arinenn@ya.ru>
2024-09-13 19:28:54 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "my_add.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        std::cerr << "Provide exactly 2 arguments." << std::endl;
+        return 1;
+    }
+
+    int a = std::atoi(argv[1]);
+    int b = std::atoi(argv[2]);
+
+    std::cout << my_add(a, b) << std::endl;
+    return 0;
+}
